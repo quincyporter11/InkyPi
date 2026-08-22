607263e126bfd5fef032ad8da8b04b959c2e4fe9
quincyporter11 <quincyporter11@gmail.com>
2026-08-22 18:25:42 -0400

    Use generated images for touchscreen dashboard

--- src/plugins/weather/weather.py
+++ src/plugins/weather/weather.py
@@ -70,12 +70,12 @@ class Weather(BasePlugin):
         }
         template_params['style_settings'] = True
         return template_params
-    def get_render_context(self, settings, device_config):
+
+    def generate_image(self, settings, device_config):
         raw_lat = settings.get('latitude', '').strip()
         raw_long = settings.get('longitude', '').strip()
         if not raw_lat or not raw_long:
             raise RuntimeError("Latitude and Longitude are required.")
-
         try:
             lat = float(raw_lat)
             long = float(raw_long)
@@ -86,238 +86,61 @@ class Weather(BasePlugin):
         if not units or units not in ['metric', 'imperial', 'standard']:
             raise RuntimeError("Units are required.")
 
-        weather_provider = settings.get(
-            'weatherProvider',
-            'OpenWeatherMap'
-        )
-
+        weather_provider = settings.get('weatherProvider', 'OpenWeatherMap')
         title = settings.get('customTitle', '')
 
-        timezone = device_config.get_config(
-            "timezone",
-            default="America/New_York"
-        )
-
-        time_format = device_config.get_config(
-            "time_format",
-            default="12h"
-        )
-
+        timezone = device_config.get_config("timezone", default="America/New_York")
+        time_format = device_config.get_config("time_format", default="12h")
         tz = pytz.timezone(timezone)
 
         try:
             if weather_provider == "OpenWeatherMap":
-                api_key = device_config.load_env_key(
-                    "OPEN_WEATHER_MAP_SECRET"
-                )
-
+                api_key = device_config.load_env_key("OPEN_WEATHER_MAP_SECRET")
                 if not api_key:
-                    raise RuntimeError(
-                        "Open Weather Map API Key not configured."
-                    )
-
-                weather_data = self.get_weather_data(
-                    api_key,
-                    units,
-                    lat,
-                    long
-                )
-
-                aqi_data = self.get_air_quality(
-                    api_key,
-                    lat,
-                    long
-                )
-
-                if settings.get(
-                    'titleSelection',
-                    'location'
-                ) == 'location':
-                    title = self.get_location(
-                        api_key,
-                        lat,
-                        long
-                    )
-
-                if settings.get(
-                    'weatherTimeZone',
-                    'locationTimeZone'
-                ) == 'locationTimeZone':
-
-                    logger.info(
-                        "Using location timezone for OpenWeatherMap data."
-                    )
-
-                    render_tz = self.parse_timezone(weather_data)
-
+                    raise RuntimeError("Open Weather Map API Key not configured.")
+                weather_data = self.get_weather_data(api_key, units, lat, long)
+                aqi_data = self.get_air_quality(api_key, lat, long)
+                if settings.get('titleSelection', 'location') == 'location':
+                    title = self.get_location(api_key, lat, long)
+                if settings.get('weatherTimeZone', 'locationTimeZone') == 'locationTimeZone':
+                    logger.info("Using location timezone for OpenWeatherMap data.")
+                    wtz = self.parse_timezone(weather_data)
+                    template_params = self.parse_weather_data(weather_data, aqi_data, wtz, units, time_format, lat)
                 else:
-                    logger.info(
-                        "Using configured timezone for OpenWeatherMap data."
-                    )
-
-                    render_tz = tz
-
-                template_params = self.parse_weather_data(
-                    weather_data,
-                    aqi_data,
-                    render_tz,
-                    units,
-                    time_format,
-                    lat
-                )
-
+                    logger.info("Using configured timezone for OpenWeatherMap data.")
+                    template_params = self.parse_weather_data(weather_data, aqi_data, tz, units, time_format, lat)
             elif weather_provider == "OpenMeteo":
                 forecast_days = 7
-
-                weather_data = self.get_open_meteo_data(
-                    lat,
-                    long,
-                    units,
-                    forecast_days + 1
-                )
-
-                aqi_data = self.get_open_meteo_air_quality(
-                    lat,
-                    long
-                )
-
-                template_params = self.parse_open_meteo_data(
-                    weather_data,
-                    aqi_data,
-                    tz,
-                    units,
-                    time_format,
-                    lat
-                )
-
+                weather_data = self.get_open_meteo_data(lat, long, units, forecast_days + 1)
+                aqi_data = self.get_open_meteo_air_quality(lat, long)
+                template_params = self.parse_open_meteo_data(weather_data, aqi_data, tz, units, time_format, lat)
             else:
-                raise RuntimeError(
-                    f"Unknown weather provider: {weather_provider}"
-                )
+                raise RuntimeError(f"Unknown weather provider: {weather_provider}")
 
             template_params['title'] = title
-
         except Exception as e:
-            logger.error(
-                f"{weather_provider} request failed: {str(e)}"
-            )
-
-            raise RuntimeError(
-                f"{weather_provider} request failure, please check logs."
-            )
-
-        template_params["plugin_settings"] = settings
-
-        now = datetime.now(tz)
-
-        if time_format == "24h":
-            last_refresh_time = now.strftime(
-                "%Y-%m-%d %H:%M"
-            )
-        else:
-            last_refresh_time = now.strftime(
-                "%Y-%m-%d %I:%M %p"
-            )
-
-        template_params["last_refresh_time"] = last_refresh_time
-
-        return template_params
-
-    def generate_image(self, settings, device_config):
-        template_params = self.get_render_context(
-            settings,
-            device_config
-        )
-
+            logger.error(f"{weather_provider} request failed: {str(e)}")
+            raise RuntimeError(f"{weather_provider} request failure, please check logs.")
+       
         dimensions = device_config.get_resolution()
-
         if device_config.get_config("orientation") == "vertical":
             dimensions = dimensions[::-1]
 
-        image = self.render_image(
-            dimensions,
-            "weather.html",
-            "weather.css",
-            template_params
-        )
+        template_params["plugin_settings"] = settings
+
+        # Add last refresh time
+        now = datetime.now(tz)
+        if time_format == "24h":
+            last_refresh_time = now.strftime("%Y-%m-%d %H:%M")
+        else:
+            last_refresh_time = now.strftime("%Y-%m-%d %I:%M %p")
+        template_params["last_refresh_time"] = last_refresh_time
+
+        image = self.render_image(dimensions, "weather.html", "weather.css", template_params)
 
         if not image:
-            raise RuntimeError(
-                "Failed to take screenshot, please check logs."
-            )
-
+            raise RuntimeError("Failed to take screenshot, please check logs.")
         return image
-
-#    def generate_image(self, settings, device_config):
-#        raw_lat = settings.get('latitude', '').strip()
-#        raw_long = settings.get('longitude', '').strip()
-#        if not raw_lat or not raw_long:
-#            raise RuntimeError("Latitude and Longitude are required.")
-#        try:
-#            lat = float(raw_lat)
-#            long = float(raw_long)
-#        except (ValueError, TypeError):
-#            raise RuntimeError("Latitude and Longitude must be valid numbers.")
-#
-#        units = settings.get('units')
-#        if not units or units not in ['metric', 'imperial', 'standard']:
-#            raise RuntimeError("Units are required.")
-#
-#        weather_provider = settings.get('weatherProvider', 'OpenWeatherMap')
-#        title = settings.get('customTitle', '')
-#
-#        timezone = device_config.get_config("timezone", default="America/New_York")
-#        time_format = device_config.get_config("time_format", default="12h")
-#        tz = pytz.timezone(timezone)
-#
-#        try:
-#            if weather_provider == "OpenWeatherMap":
-#                api_key = device_config.load_env_key("OPEN_WEATHER_MAP_SECRET")
-#                if not api_key:
-#                    raise RuntimeError("Open Weather Map API Key not configured.")
-#                weather_data = self.get_weather_data(api_key, units, lat, long)
-#                aqi_data = self.get_air_quality(api_key, lat, long)
-#                if settings.get('titleSelection', 'location') == 'location':
-#                    title = self.get_location(api_key, lat, long)
-#                if settings.get('weatherTimeZone', 'locationTimeZone') == 'locationTimeZone':
-#                    logger.info("Using location timezone for OpenWeatherMap data.")
-#                    wtz = self.parse_timezone(weather_data)
-#                    template_params = self.parse_weather_data(weather_data, aqi_data, wtz, units, time_format, lat)
-#                else:
-#                    logger.info("Using configured timezone for OpenWeatherMap data.")
-#                    template_params = self.parse_weather_data(weather_data, aqi_data, tz, units, time_format, lat)
-#            elif weather_provider == "OpenMeteo":
-#                forecast_days = 7
-#                weather_data = self.get_open_meteo_data(lat, long, units, forecast_days + 1)
-#                aqi_data = self.get_open_meteo_air_quality(lat, long)
-#                template_params = self.parse_open_meteo_data(weather_data, aqi_data, tz, units, time_format, lat)
-#            else:
-#                raise RuntimeError(f"Unknown weather provider: {weather_provider}")
-#
-#            template_params['title'] = title
-#        except Exception as e:
-#            logger.error(f"{weather_provider} request failed: {str(e)}")
-#            raise RuntimeError(f"{weather_provider} request failure, please check logs.")
-#       
-#        dimensions = device_config.get_resolution()
-#        if device_config.get_config("orientation") == "vertical":
-#            dimensions = dimensions[::-1]
-#
-#        template_params["plugin_settings"] = settings
-#
-#        # Add last refresh time
-#        now = datetime.now(tz)
-#        if time_format == "24h":
-#            last_refresh_time = now.strftime("%Y-%m-%d %H:%M")
-#        else:
-#            last_refresh_time = now.strftime("%Y-%m-%d %I:%M %p")
-#        template_params["last_refresh_time"] = last_refresh_time
-#
-#        image = self.render_image(dimensions, "weather.html", "weather.css", template_params)
-#
-#        if not image:
-#            raise RuntimeError("Failed to take screenshot, please check logs.")
-#        return image
 
     def parse_weather_data(self, weather_data, aqi_data, tz, units, time_format, lat):
         current = weather_data.get("current")
